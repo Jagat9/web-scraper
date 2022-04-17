dbe24ef4cbbe971cc34830070d3ff27917bb6078
Jagat Jeeban Basantia <47893550+Jagat9@users.noreply.github.com>
2022-04-17 13:48:03 +0000

    adding old code to old.py

---
+++ old.py
@@ -0,0 +1,25 @@
+
+import requests
+
+from bs4 import BeautifulSoup
+
+
+youtube_trending_page = 'https://www.youtube.com/feed/trending'
+#does not excute javescript
+
+response = requests.get(youtube_trending_page)
+
+print('status Code', response.status_code)
+print("output", response.text[:1000])
+
+
+with open('trending.html', 'w') as f:
+  f.write(response.text)
+
+
+doc = BeautifulSoup(response.text, 'html.parser')
+print('page.title:',doc.title)
+
+video_divs= doc.find_all('div', class_='ytd-video-renderer')
+
+print(f'found{len(video_divs)}videos')
--- scraper.py
+++ scraper.py
@@ -1,24 +1,11 @@
 
-import requests
-
-from bs4 import BeautifulSoup
-
+from selenium import webdriver
 
 youtube_trending_page = 'https://www.youtube.com/feed/trending'
 
-response = requests.get(youtube_trending_page)
+driver = webdriver.Chrome()
 
-print('status Code', response.status_code)
-print("output", response.text[:1000])
+driver.get(youtube_trending_page)
 
+print('page title', driver.title)
 
-with open('trending.html', 'w') as f:
-  f.write(response.text)
-
-
-doc = BeautifulSoup(response.text, 'html.parser')
-print('page.title:',doc.title)
-
-video_divs= doc.find_all('div', class_='ytd-video-renderer')
-
-print(f'found{len(video_divs)}videos')
