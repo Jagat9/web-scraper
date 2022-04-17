684b7e6bf9f5da8559870bfd4f92f09db9ebde92
Jagat Jeeban Basantia <47893550+Jagat9@users.noreply.github.com>
2022-04-17 10:50:40 +0000

    till bs4

--- scraper.py
+++ scraper.py
@@ -1,1 +1,24 @@
-print('hello world')
+
+import requests
+
+from bs4 import BeautifulSoup
+
+
+youtube_trending_page = 'https://www.youtube.com/feed/trending'
+
+response = requests.get(youtube_trending_page)
+
+print('status Code', response.status_code)
+print("output", response.text[:1000])
+
+
+with open('trending.html', 'w') as f:
+  f.write(response.text)
+
+
+doc = BeautifulSoup(response.text, 'html.parser')
+print('page.title:',doc.title)
+
+video_divs= doc.find_all('div', class_='ytd-video-renderer')
+
+print(f'found{len(video_divs)}videos')
